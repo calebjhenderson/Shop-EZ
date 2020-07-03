23357d39cc7682fac1d0159ba462dc8a095ba1c9
yhafez <yhafez3@gmail.com>
2020-07-02 22:22:14 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, './src/app.js'),
+    output: {
+        filename: 'main.js',
+        path: path.join(__dirname, './public'),
+    },
+    node: {
+        fs: "empty"
+     },
+    devtool: 'source-map',
+    module: {
+        rules: [
+
+            {
+            use: {
+                loader: 'babel-loader'
+            },
+            exclude: /node_nodules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                // Creates `style` nodes from JS strings
+                'style-loader',
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader',
+                ],
+            },
+        ]
+    }
+};
