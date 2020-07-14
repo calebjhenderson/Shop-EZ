bcd23358f4ef1626663afe683ac386fdb5eb71a8
tdyleuth <tony.dyleuth@gmail.com>
2020-07-13 19:54:00 -0700

    Pushing

---
+++ src/components/StoreContent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core/";
+
+const useStyles = makeStyles({
+  storeContent: {
+    marginRight: 150,
+    marginLeft: 150,
+    padding: 30,
+    background: "#3d2f75",
+  },
+});
+
+function StoreContent() {
+  const classes = useStyles();
+  return (
+    <Grid className={classes.storeContent}>
+      <ProductCard />
+    </Grid>
+  );
+}
+
+export default StoreContent;
---
+++ src/components/dummy-products.js
@@ -0,0 +1,57 @@
+const products = [
+  {
+    title: "Product 1",
+    excerpt: "This is my first item with more content inside",
+    price: "$9.99",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+
+  {
+    title: "Product 2",
+    excerpt: "This is my second item with more content inside",
+    price: "$9.99",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+
+  {
+    title: "Product 3",
+    excerpt: "This is my third item with more content inside",
+    price: "$9.99",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+
+  {
+    title: "Product 4",
+    excerpt: "This is my fourth item with more content inside",
+    price: "$9.99",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+
+  {
+    title: "Product 5",
+    excerpt: "This is my fifth item with more content inside",
+    price: "$9.99",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+
+  {
+    title: "Product 6",
+    excerpt: "This is my sixth item with more content inside",
+    price: "$9.99",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+  {
+    title: "Product 7",
+    excerpt: "This is my sixth item with more content inside",
+    price: "$9.99",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+  {
+    title: "Product 8",
+    excerpt: "This is my sixth item with more content inside",
+    price: "$9.99",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+];
+
+export default products;
